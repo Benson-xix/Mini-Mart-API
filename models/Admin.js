aff4ba4f9845d463a93dc1668eba1059d1a617fb
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-01-08 17:17:46 +0100

    Update

--- models/Admin.js
+++ models/Admin.js
@@ -19,7 +19,7 @@ const adminSchema = new mongoose.Schema({
 
     pic: {
         type: String,
-        default: "https://mini-mart-api.herokuapp.com/img/user.png"
+        default: "https://mini-mart-api.onrender.com/img/user.png"
     }
 
 
